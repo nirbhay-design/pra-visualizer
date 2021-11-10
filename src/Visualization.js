2dcf512bf45687dfe642bc7e7e3c04b7a133b20d
Mayank <mynksrjpt@gmail.com>
2021-11-10 18:08:38 +0530

    added more features

--- src/Visualization.js
+++ src/Visualization.js
@@ -122,7 +122,7 @@ function Visualization({frames,refstr,alggo}) {
     //var m=Number(frames);
     return (
         <div className="visualization__algorithms">
-            <center><h1> Visualizer</h1></center>
+            <center><h1> Visualizer </h1></center>
             <br/>
             <table className="tablemy">
                 {arr1.map((items,index)=>{
@@ -135,13 +135,16 @@ function Visualization({frames,refstr,alggo}) {
                 
             </table>
             <br/>
-            
-            <li>Frames are : {(frames.length >0)?frames:"nothing to show here"} <br/></li>
-            <li>Reference String: {(refstr.length > 0)?refstr:"nothing to show here"}<br/></li>
-            <li> Algorithm : {(alggo.length >0)?alggo:"nothing to show here"}<br/></li>
-            <li>No. of Page Faults : {miss}<br/></li>
-            <li>Page Faults percentage : {per(miss,arr2.length)}%<br/></li>
-            <li>Hit rate percentage: {100-per(miss,arr2.length)}%<br/></li>
+
+            {/* <div className="infoText"> */}
+            <h2> Info </h2>
+            <li>Frames are : {(frames.length >0)?frames:"nothing to show here"} <span class="extra"> These are the number of frames that are provided by the user. User can change it's value to get variation in results!</span> <br/></li>
+            <li>Reference String: {(refstr.length > 0)?refstr:"nothing to show here"} <span class="extra"> Another attribute provided by the user. Change the reference string to get the results for different test cases.</span> <br/></li>
+            <li>Algorithm : {(alggo.length >0)?alggo:"nothing to show here"} <span class="extra"> It shows, which algorithm is currently in use in the backend!</span> <br/></li>
+            <li>No. of Page Faults : {miss} <span class="extra"> A page fault occurs when a program attempts to access a block of memory that is not stored in the physical memory, or RAM. The fault notifies the operating system that it must locate the data in virtual memory, then transfer it from the storage device, such as an HDD or SSD, to the system RAM.</span> <br/></li>
+            <li>Page Faults percentage : {per(miss,arr2.length)}% <span class="extra"> It shows the percentage of total pages which resulted in page faults or page misses.</span> <br/></li>
+            <li>Hit rate percentage : {100-per(miss,arr2.length)}% <span class="extra"> It is the percentage of total pages which are in the memory when we access them i,e, the percentage of HIT pages.</span> <br/></li>
+            {/* </div> */}
            
         </div>
     )
